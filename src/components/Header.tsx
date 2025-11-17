efef1b06dd15cc04bc97390a66e8bf5cd3563706
Pedroguibas <pedroguibas123@gmail.com>
2025-11-17 10:10:37 -0300

    feat: creates Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import "../assets/css/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-container">
+        <button className="header-brand-container">
+          <img
+            src="/DnD-brand.webp"
+            alt="Logo Dungeons and Dragons"
+            className="header-brand"
+          />
+        </button>
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <button className="navbar-button">Minhas Fichas</button>
+            </li>
+            <li className="navbar-item">
+              <button className="navbar-button">Meu Perfil</button>
+            </li>
+            <li className="navbar-item">
+              <button className="navbar-button login-button">Entrar</button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <section>
+      <h1>login</h1>
+    </section>
+  );
+};
+
+export default Login;
